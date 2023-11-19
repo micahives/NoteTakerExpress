53e7c20555ac7dfa6c8eed45fd6c8895ec2b0b45
Micah Ives <micbenives@gmail.com>
2023-11-18 20:19:07 -0600

    Update app.post to append db.json

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const path = require('path');
+// I may not want to require this as it keeps changing, and loads...
+// the cached version if you require changes
 const notesData = require('./Develop/db/db.json');
 const uuid = require('./Develop/helpers/uuid.js');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+function jsonReader(filePath, cb) {
+    fs.readFile(filePath, 'utf-8', (err, fileData) => {
+        if (err) {
+            return cb && cb(err);
+        }
+        try {
+            const object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        } catch (err) {
+            return cb && cb(err);
+        }
+    });
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -28,7 +45,6 @@ app.get('/api/notes', (req, res) =>
 );
 
 // POST route for save new note and return updated db.json (API route)
-// TODO: give each note a unique ID when saved
 app.post('/api/notes', (req, res) => {
         // log that POST request was received
         console.info(`${req.method} request received to add a note`);
@@ -41,15 +57,36 @@ app.post('/api/notes', (req, res) => {
                 title,
                 text,
                 id: uuid(),
-            };
+            };    
 
-            const response = {
-                status: 'success',
-                body: newNote,
-            };
+            jsonReader('./Develop/db/db.json', (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
 
-            console.log(response);
-            res.status(200).json(response);
+                    data.push(newNote)
+
+                    const newNoteString = JSON.stringify(data, null, 2);
+
+                    fs.writeFile('./Develop/db/db.json', newNoteString, err => {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).json('Error in posting note');
+                        } else {
+                            console.log('New data added');
+
+                            const response = {
+                                status: 'success',
+                                body: newNote,
+                            };
+
+                            console.log(response);
+                            res.status(200).json(response);
+                        }
+                    });
+                }
+            });
+
         } else {
             res.status(500).json('Error in posting note');
         }
